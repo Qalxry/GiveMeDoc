3a89abc9c26657f4d5a453ccff6f1db13ef9a509
Qalxry <qalxry@qq.com>
2026-02-26 23:31:36 +0800

    feat: 更新图标生成脚本，添加生成不同尺寸图标的功能；修改SVG图标颜色和透明度

---
+++ scripts/build_gmd_icon.py
@@ -0,0 +1,15 @@
+import cairosvg
+import os
+
+with open(os.path.join(os.path.dirname(__file__), '../public/icons/gmd.svg'), 'r') as f:
+  svg = f.read()
+
+for size in [16, 48, 128]:
+    cairosvg.svg2png(
+        bytestring=svg.encode('utf-8'),
+        # write_to=f'icon-{size}.png',
+        write_to=os.path.join(os.path.dirname(__file__), f'../public/icons/icon-{size}.png'),
+        output_width=size,
+        output_height=size
+    )
+    print(f'✓ Generated icon-{size}.png')
